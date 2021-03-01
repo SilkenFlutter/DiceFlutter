a812a3801f66b4f54ae1c5047e7477a1f836d6b8
Bryan Palma <bryanmauriciopalma@hotmail.com>
2021-02-28 18:46:58 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+class _DicePageState extends State<DicePage> {
+  int numeroDadoIzquierdo = 1;
+  int numeroDadoDerecho = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+                  onPressed: (){
+                    setState(() {
+                      numeroDadoIzquierdo=GetRandomNumber();
+                    });
+                  },
+                  child: Image.asset('images/dice$numeroDadoIzquierdo.png')
+              )
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: (){
+                  setState(() {
+                    numeroDadoDerecho= GetRandomNumber();
+                  });
+                },
+                child: Image.asset('images/dice$numeroDadoDerecho.png')
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
+
+int GetRandomNumber(){
+  Random rng = Random();
+  var num = rng.nextInt(6)+1;
+  return num;
 }
